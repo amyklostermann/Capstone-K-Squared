8c311d9afa44de01e9ac422044030f33c6604615
Amy Klostermann <amyklostermann@yahoo.com>
2020-09-02 17:43:03 -0500

    Update header tags

--- components/views/About.js
+++ components/views/About.js
@@ -2,21 +2,21 @@ const winstonBowtie = require('../../Docs/Photos/Winston.jpg')
 const jason = require('../../Docs/Photos/Jason.jpg')
 
 export default() => `
-<h1><b>Jason's Story</b></h1>
+<h2><b>Jason's Story</b></h2>
   <img src="${jason}" class="blur" id="jasonimg" alt="Jason Klostermann "/>
 <div id="jasonintro">
-   <h2>Dr. Jason Klostermann, D.C.</h2>
-  <h3>Brother, Husband, Son, Step-Father, and Friend to Many </h3>
+   <h3>Dr. Jason Klostermann, D.C.</h3>
+  <h4>Brother, Husband, Son, Step-Father, and Friend to Many </h4>
 </div>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sagittis nulla. Etiam semper mauris a quam tempus, non feugiat massa posuere. Morbi vitae nisi aliquam, semper diam id, molestie leo. In hac habitasse platea dictumst. Nulla et enim vel elit dictum viverra. Nunc pharetra lacinia arcu sit amet dignissim. Pellentesque ut mi nulla.</p>
     <p>Quisque at hendrerit libero, eget interdum lectus. Etiam facilisis leo nulla, sit amet imperdiet nunc molestie vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur consectetur felis a purus volutpat, sed finibus magna iaculis. Pellentesque tristique tristique turpis nec vehicula. Maecenas varius quis tellus id mollis. Vivamus ut ultrices ligula.</p>
     <p>Etiam egestas consectetur gravida. Nulla mollis suscipit sapien sed fermentum. Integer vitae eros a magna vulputate aliquam. Suspendisse sed pulvinar augue, auctor mollis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eleifend diam quis libero sollicitudin efficitur. Nullam sapien eros, tempor eget vulputate ut, interdum vel orci. Donec sit amet tempor mi. Nam feugiat cursus egestas. Suspendisse eget orci et ex mattis ornare tempor non tellus. Suspendisse gravida neque in urna congue bibendum. Duis dui odio, pharetra nec odio ac, ornare vulputate nibh.</p>
 <br>
-<h1><b>Leadership</b></h1>
+<h2><b>Leadership</b></h2>
     <img src="${winstonBowtie}" class="blur" id="reactIMG" alt="Sir Barnibus Winston, III"/>
 <div id="winstonintro">
-    <h2>Sir Barnibus Winston, III</h2>
-    <h3>CEO and President </h3>
+    <h3>Sir Barnibus Winston, III</h3>
+    <h4>CEO and President </h4>
 </div>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sagittis nulla. Etiam semper mauris a quam tempus, non feugiat massa posuere. Morbi vitae nisi aliquam, semper diam id, molestie leo. In hac habitasse platea dictumst. Nulla et enim vel elit dictum viverra. Nunc pharetra lacinia arcu sit amet dignissim. Pellentesque ut mi nulla.</p>
     <p>Quisque at hendrerit libero, eget interdum lectus. Etiam facilisis leo nulla, sit amet imperdiet nunc molestie vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur consectetur felis a purus volutpat, sed finibus magna iaculis. Pellentesque tristique tristique turpis nec vehicula. Maecenas varius quis tellus id mollis. Vivamus ut ultrices ligula.</p>
