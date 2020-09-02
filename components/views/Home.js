ed4dda5bb688d07206f9e553bf40733396771e07
Amy Klostermann <amyklostermann@yahoo.com>
2020-09-02 17:43:54 -0500

    Add pics and update headers

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,26 @@
+const DogGirlLaughing = require('../../Docs/Photos/DogGirlLaughing.jpg')
+const HeadToHead = require('../../Docs/Photos/HeadToHead.jpg')
+const WomanDogHug = require('../../Docs/Photos/WomanDogHug.jpg')
+const ChildDog = require('../../Docs/Photos/ChildDog.jpg')
+const TwoDogRescue = require('../../Docs/Photos/TwoDogRescue.jpg')
+
 export default () => `
-<section id="jumbotron">
-<div class="homeHeaders">
+<section id="jumbotron1">
+    <img src="${ChildDog}" class="blur" id="cdimg" alt="Child Gently Laying on Dog"/>
+</section><br>
+<section id="jumbotron2">
+    <img src="${HeadToHead}" class="blur" id="h2himg" alt="Head To Head"/>
+</section><br>
+<section id="jumbotron3">
+    <img src="${DogGirlLaughing}" class="blur" id="childdogimg" alt="Girl Playing with Two Dogs"/>
+</section><br>
+<section id="jumbotron4">
+    <img src="${WomanDogHug}" class="blur" id="wdhimg" alt="Woman Hugging Dog"/>
+</section><br>
+<section id="jumbotron5">
+    <img src="${TwoDogRescue}" class="blur" id="tdrimg" alt="Two Dogs at Animal Rescue"/>
+</section><br>
+<div class="homeMIHeaders">
         <h2>Mission Statement</h2>
 </div>
     <div class="homeStatements">
@@ -14,11 +34,10 @@ export default () => `
         <p>Purr while eating. Munch, munch, chomp, chomp lick human with sandpaper tongue. Claw drapes meow loudly just to annoy owners, lick arm hair. Mouse dead stare with ears cocked there's a forty year old lady there let us feast, and fish i must find my red catnip fishy fish if human is on laptop sit on the keyboard. Make plans to dominate world and then take a nap refuse to drink water except out of someone's glass so rub my belly hiss chew on cable found somthing move i bite it tail. Slap owner's face at 5am until human fills food dish stinky cat immediately regret falling into bathtub but the cat was chasing the mouse so when owners are asleep, cry for no apparent reason. Curl into a furry donut jump five feet high and sideways when a shadow moves, or tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad stares at human while pushing stuff off a table more napping, more napping all the napping is exhausting.</p>
     </div>
 <br>
-<div class="homeHeaders">
+<div class="homeSumHeaders">
         <h2>Summary of What We Do</h2>
 </div>
     <div class="homeStatements">
         <p>Purr while eating. Munch, munch, chomp, chomp lick human with sandpaper tongue. Claw drapes meow loudly just to annoy owners, lick arm hair. Mouse dead stare with ears cocked there's a forty year old lady there let us feast, and fish i must find my red catnip fishy fish if human is on laptop sit on the keyboard. Make plans to dominate world and then take a nap refuse to drink water except out of someone's glass so rub my belly hiss chew on cable found somthing move i bite it tail. Slap owner's face at 5am until human fills food dish stinky cat immediately regret falling into bathtub but the cat was chasing the mouse so when owners are asleep, cry for no apparent reason. Curl into a furry donut jump five feet high and sideways when a shadow moves, or tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad stares at human while pushing stuff off a table more napping, more napping all the napping is exhausting.</p>
-    </div>
-</section>
+    </div> 
 `;
