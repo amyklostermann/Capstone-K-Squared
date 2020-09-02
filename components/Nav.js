cb5be1509fdf126f7b4a8f37dc109fc4f4fa2d4a
Amy Klostermann <amyklostermann@yahoo.com>
2020-09-02 17:40:28 -0500

    Hid hamburger icon for now

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,13 @@
 export default (links) => `
 <nav>
 
-<i class="fas fa-bars"></i>
-
 <ul class="hidden--mobile nav-links">
   ${links.map(link => `<li><a href="/${(link.title)}" data-navigo>${link.text}</a></li>`).join("")}
 </ul>
 </nav>
 `;
 
-
+//Hiding for now
+//<i class="fas fa-bars"></i>
+//
 
